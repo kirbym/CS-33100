1e151203f90b6cab348166f02a93bd0c2f345af3
Michael Kirby <kirbyma729@gmail.com>
2016-05-18 22:52:39 -0400

    5/18/16
    GradeBook class definition header file
    Use with:
    - GradeBook.cpp

---
+++ GradeBook.h
@@ -0,0 +1,30 @@
+// Gradebook.h
+// Header file serves to define the class "GradeBook"
+// This file also defines the constructors as well as the member functions with function prototypes
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+// using namespace std; (not supposed to include this)
+// thereby write std::cout, std::string, std::endl, etc.
+
+class GradeBook // class definition
+{
+public:
+	// constructor initializes GradeBook object without courseName
+	explicit GradeBook();
+
+	// constructor initializes courseName with string supplied as argument
+	explicit GradeBook(std::string name);
+
+	// function that sets the course name
+	void setCourseName(std::string name);
+
+	// function that gets the course name
+	std::string getCourseName();
+
+	// function that displays a welcome message to the GradeBook user
+	void displayMessage();
+private:
+	std::string courseName; // course name for this GradeBook
+}; // end class GradeBook
